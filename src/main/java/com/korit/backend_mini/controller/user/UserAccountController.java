f0b731f84092c6b4104ed95284d0ca9a4bf0051f
KimCoal <121536823+KimCoal@users.noreply.github.com>
2025-12-30 09:01:23 +0900

    Feat : getPrincipal

--- src/main/java/com/korit/backend_mini/controller/user/UserAccountController.java
+++ src/main/java/com/korit/backend_mini/controller/user/UserAccountController.java
@@ -2,21 +2,24 @@ package com.korit.backend_mini.controller.user;
 
 import com.korit.backend_mini.dto.account.ModifyPasswordReqDto;
 import com.korit.backend_mini.dto.account.ModifyUsernameReqDto;
+import com.korit.backend_mini.dto.response.ApiRespDto;
 import com.korit.backend_mini.security.model.Principal;
 import com.korit.backend_mini.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user/account")
 @RequiredArgsConstructor
 public class UserAccountController {
     private final AccountService accountService;
+
+    @GetMapping("/principal")
+    public ResponseEntity<?> getPrincipal(@AuthenticationPrincipal Principal principal) {
+        return ResponseEntity.ok(new ApiRespDto<>("success", "회원 조회 완료", principal));
+    }
 
     @PostMapping("/modify/password")
     public ResponseEntity<?> modifyPassword (@RequestBody ModifyPasswordReqDto modifyPasswordReqDto,
@@ -34,6 +37,5 @@ public class UserAccountController {
     public ResponseEntity<?> withdraw(@AuthenticationPrincipal Principal principal) {
         return ResponseEntity.ok(accountService.withdraw(principal));
     }
-
 
 }
